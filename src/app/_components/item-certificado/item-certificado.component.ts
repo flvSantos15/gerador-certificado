228f4f5ec6a95d4807278b609f0a0cf62c016f03
flvSantos15 <flvsantos300@gmail.com>
2025-07-25 07:33:08 -0300

    chore: add item certificado

---
+++ src/app/_components/item-certificado/item-certificado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-certificado',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-certificado.component.html',
+  styleUrl: './item-certificado.component.css'
+})
+export class ItemCertificadoComponent {
+
+}
