7ca0802ac4d81f411433cecd5325c04aa7f2a6c2
flvSantos15 <flvsantos300@gmail.com>
2025-07-31 09:16:57 -0300

    chore: add certificado-form

---
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
+
+@Component({
+  selector: 'app-certificado-form',
+  standalone: true,
+  imports: [SecondaryButtonComponent, PrimaryButtonComponent],
+  templateUrl: './certificado-form.component.html',
+  styleUrl: './certificado-form.component.css'
+})
+export class CertificadoFormComponent {
+
+}
