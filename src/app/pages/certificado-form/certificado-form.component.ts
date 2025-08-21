cf88bb4843e1cd88cbf8459c21f5a22eaa57319b
flvSantos15 <flvsantos300@gmail.com>
2025-08-21 00:23:49 -0300

    chore: add activities

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+import { FormsModule } from '@angular/forms';
 import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
+import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
 
 @Component({
   selector: 'app-certificado-form',
   standalone: true,
-  imports: [SecondaryButtonComponent, PrimaryButtonComponent],
+  imports: [
+    SecondaryButtonComponent,
+    PrimaryButtonComponent,
+    FormsModule
+  ],
   templateUrl: './certificado-form.component.html',
   styleUrl: './certificado-form.component.css'
 })
 export class CertificadoFormComponent {
-
+  nome: string = ''
+  atividade: string = ''
+  atividades: string[] = []
 }
