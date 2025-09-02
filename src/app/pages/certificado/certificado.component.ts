ab27a84f438db4bfa6317040704561fb7dc05347
flvSantos15 <flvsantos300@gmail.com>
2025-09-01 23:45:49 -0300

    chore: add final adjusments

--- src/app/pages/certificado/certificado.component.ts
+++ src/app/pages/certificado/certificado.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, RouterLink } from "@angular/router";
+import html2canvas from 'html2canvas';
 import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
 import { CertificadoService } from '../../_services/certificado.service';
 import { ICertificado } from '../../interfaces/certificado';
@@ -15,6 +16,8 @@ export class CertificadoComponent implements OnInit {
   id: string | null = null
   certificado: ICertificado | undefined
 
+  @ViewChild('certificadoContainer') certificadoContainer!: ElementRef
+
   constructor(
     private certificadoService: CertificadoService,
     private route: ActivatedRoute
@@ -26,5 +29,19 @@ export class CertificadoComponent implements OnInit {
       this.certificado = this.certificadoService.certificados
         .find((item) => item.id === this.id)
     })
+  }
+
+  downloadCertificado() {
+    if (this.certificado == undefined) {
+      return
+    }
+    html2canvas(this.certificadoContainer.nativeElement, { scale: 2 }).then(
+      canvas => {
+        const link = document.createElement('a')
+        link.href = canvas.toDataURL('image/png')
+        link.download = 'certificado_' + this.certificado?.id.replaceAll(' ', '_') + '.png'
+        link.click()
+      }
+    )
   }
 }
